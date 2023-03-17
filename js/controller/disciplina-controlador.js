b95685498568266d4b261553680a60d77a414e1b
Manoel Pedro <80185451+mnlpdr@users.noreply.github.com>
2023-03-17 03:08:59 -0300

    Update disciplina-controlador.js

--- js/controller/disciplina-controlador.js
+++ js/controller/disciplina-controlador.js
@@ -8,8 +8,49 @@ class DisciplinaControlador {
         const codigoDisciplina = Number(document.getElementById('codigoDisciplina').value);
         const nomeDisciplina = document.getElementById('nomeDisciplina').value;
         const inserirDisciplina = this.disciplinaServico.inserir(codigoDisciplina, nomeDisciplina);
-        if (inserirDisciplina) {
-            
+        if (disciplina) {
+            this.mostrarDisciplina(nomeDisciplina, codigoDisciplina);
+            alert('Disciplina inserida com sucesso');
+        } else {
+            alert('Erro. Verifique se a disciplina já foi cadastrada anteriormente');
         }
-    
+            
+    }
+        
+    mostrarDisciplina(nome, codigo) {
+        const elementoP = document.createElement("p");
+        elementoP.textContent = `${nome} - ${codigo}`;
+
+        const elementoBotaoApagar = document.createElement("button");
+        elementoBotaoApagar.textContent = "X";
+
+        elementoBotaoApagar.addEventListener('click', (event) => {
+                this.removerDisciplina(codigo);
+                event.target.parentElement.remove();
+            }
+        );
+        elementoP.appendChild(elementoBotaoApagar);
+        document.body.appendChild(elementoP);
+    }
+
+    removerDisciplina(codigo){
+        this.disciplinaServico.remover(codigo);
+    }
+
+    inserirAlunoNaDisciplina() {
+        const aluno = new Aluno(nome, idade);
+        this.disciplinaServico.inserirAlunoNaDisciplina(aluno, codigo);
+        if(aluno){
+            alert('Aluno inserido com sucesso');
+        }
+        else{
+            alert('Erro. Verifique se o aluno já foi cadastrado anteriormente');
+        }
+        
+    }
+
+    /*removerAlunoDisciplina(codigo, nome) {
+        //IMPLEMENTAR 
+
+    }*/
 }
